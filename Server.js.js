702ecc35c3f2e70ee168c767d5ebb6d2c2903994
Brandon Sakai <brandon.sakai@gmail.com>
2019-09-18 02:11:25 +0000

    Switched to MySQL

--- Server.js.js
+++ Server.js.js
@@ -1,7 +1,7 @@
 //Initiallising node modules
 var express = require("express");
 var bodyParser = require("body-parser");
-var sql = require("mssql");
+var mysql = require("mysql");
 var app = express();
 
 // Setting Base directory
@@ -22,68 +22,58 @@ app.use(function (req, res, next) {
     console.log("App now running on port", port);
  });
 
-//Initiallising connection string
-var dbConfig = {
-    user:  "developer",
-    password: "developer",
-    server: "automaticdoorctrldev.cap2cfj45nxa.us-east-1.rds.amazonaws.com",
-    database: "users"
-};
+//Initiallising connection
+var connection = mysql.createConnection({
+	user:		"developer",
+	password:	"developer",
+	host:		"192.168.56.103",
+	database:	"users"
+});
 
 //Function to connect to database and execute query
 var  executeQuery = function(res, query){	
-	sql.connect(dbConfig, function (err) {
-		if (err) {   
-			console.log("Error while connecting database :- " + err);
-			res.send(err);
+	connection.query(query, function (error, results, fields) {
+		if (error) {
+			console.log("Database error : " + error);
+			res.send(error);
 		}
 		else {
-			// create Request object
-			var request = new sql.Request();
-			// query to the database
-			request.query(query, function (err, rs) {
-				if (err) {
-					console.log("Error while querying database :- " + err);
-					res.send(err);
-				}
-				else {
-					console.log("what the server is sending back: ",rs);
-					res.send(rs);
-				}
-			});
+			console.log("Database returned: ", results);
+			res.send(results);
 		}
-	});	
+	});
 }
 
 // get active student data **
 app.get("/api/active_user", function(req , res){
-	var query = "select * from [students] where TRIM(LOWER(Status)) = 'active' ";
+	var query = "select * from students where Status = 'Active' ";
 	executeQuery (res, query);
 });
 
 // get student request data **
 app.get("/api/inactive_user", function(req , res){
-	var query = "select * from [students] where TRIM(LOWER(Status)) = 'request' ";
+	var query = "select * from students where Status = 'Request' ";
 	executeQuery (res, query);
 });
 
 //add all student requests to active**
 app.get("/api/addAll", function(req , res){
-	var query = "UPDATE [students] SET Status = 'Active' WHERE TRIM(LOWER(Status)) = 'request'";
+	var query = "UPDATE students SET Status = 'Active' WHERE Status = 'Request'";
 	executeQuery (res, query);
 });
 
 // login to the app  for students**
 app.post("/api/login", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "select * from [students] where RCSid = '" + req.body.RCSid+"'" ;
+	var query = "select * from students where RCSid = " + mysql.escape(req.body.RCSid);
 	console.log(query);
 	executeQuery (res, query);
 });
+
 // login to the app  for admin**
 app.post("/api/admin/login", function(req , res){
 	console.log(req.body.username);
-	var query = "select * from [admin] where username = '" + req.body.username+"' and password = '" + req.body.password+"'" ;
+	var query = "select * from admin where username = " + mysql.escape(req.body.username) + " and password = " + mysql.escape(req.body.password);
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -91,7 +81,7 @@ app.post("/api/admin/login", function(req , res){
 //register for the app ** 
 app.post("/api/request-access", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "INSERT INTO [students] (RCSid,Status) VALUES ('"+req.body.RCSid+"', 'Request')";
+	var query = "INSERT INTO students (RCSid,Status) VALUES (" + mysql.escape(req.body.RCSid) + ", 'Request')";
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -99,7 +89,7 @@ app.post("/api/request-access", function(req , res){
 //add singular student to active status **
 app.post("/api/addtoActive", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "UPDATE [students] SET Status = 'Active' WHERE RCSid = '" + req.body.RCSid+"'";
+	var query = "UPDATE students SET Status = 'Active' WHERE RCSid = " + mysql.escape(req.body.RCSid);
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -107,7 +97,7 @@ app.post("/api/addtoActive", function(req , res){
 //remove a student from active access **
 app.post("/api/remove", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "DELETE FROM [students] WHERE RCSid = '" + req.body.RCSid+"'";
+	var query = "DELETE FROM students WHERE RCSid = " + mysql.escape(req.body.RCSid);
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -115,13 +105,14 @@ app.post("/api/remove", function(req , res){
 //submit a location complaint **
 app.post("/api/submit-complaint", function(req , res){
 	console.log(req.body.location);
-	var query = "INSERT INTO [complaints] (location,message) VALUES ('"+req.body.Location+"', '"+req.body.Message+"')";
+	var query = "INSERT INTO complaints (location,message) VALUES (" + mysql.escape(req.body.Location) + ", " + mysql.escape(req.body.Message) + ")";
 	console.log(query);
 	executeQuery (res, query);
 });
 
 // list complaints made
 app.get("/api/get-complaints", function(req , res){
-	var query = "select * from [complaints] ";
+	var query = "select * from complaints";
 	executeQuery (res, query);
 });
+
